88fbff72a3da64b6c56f94fc114f2fba332c8fe9
Irfan Mulic <irfan1@Irfans-MacBook-Pro.local>
2015-11-03 15:57:50 -0800

    added new routes for testing pages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'KuleReader' });
 });
 
+router.get('/testindex', function(req, res) {
+  res.render('testindex', { title: 'Before Retention Quiz' });
+});
+
+router.get('/test', function(req, res) {
+  res.render('test', { title: 'Retention Quiz' });
+});
+
+
 router.get('/page/:number', function(req, res) {
   res.render('page/' + req.params.number, { title: 'KuleReader' });
 });
